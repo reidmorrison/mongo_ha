56e758da4c32a469bba7dfcce36c3d19cefa7eb4
Reid Morrison <reidmo@gmail.com>
2017-10-24 12:14:57 -0400

    Support mongo v2.5.0.beta

--- lib/mongo_ha/retryable.rb
+++ lib/mongo_ha/retryable.rb
@@ -16,45 +16,25 @@ module Mongo
       rescue Error::OperationFailure => e
         raise(e) if !e.retryable? || (attempt > cluster.max_read_retries)
         log_retry(e)
-        if cluster.sharded?
-          # We don't scan the cluster in this case as Mongos always returns
-          # ready after a ping no matter what the state behind it is.
-          sleep(cluster.read_retry_interval)
-        else
-          cluster.scan!
-        end
+        cluster.sharded? ? sleep(cluster.read_retry_interval) : cluster.scan!
         retry
       end
     end
 
-    def write_with_retry
+    def write_with_retry(session, server_selector)
       attempt = 0
       begin
         attempt += 1
-        yield
+        yield(server_selector.call)
       rescue Error::SocketError, Error::SocketTimeoutError => e
         raise(e) if attempt > cluster.max_read_retries
         log_retry(e)
-        # During a replicaset master change the primary immediately closes all existing client connections.
-        #
-        # Note:
-        #   Small possibility the write occurs twice.
-        #   Usually this is acceptable since most applications would just retry the write anyway.
-        #   The ideal way is to check if the write succeeded, or just use a primary key to
-        #   prevent multiple writes etc.
-        #   In production we have not seen duplicates using this retry mechanism.
         cluster.scan!
         retry
       rescue Error::OperationFailure => e
         raise(e) if !e.write_retryable? || (attempt > cluster.max_read_retries)
         log_retry(e)
-        if cluster.sharded?
-          # We don't scan the cluster in this case as Mongos always returns
-          # ready after a ping no matter what the state behind it is.
-          sleep(cluster.read_retry_interval)
-        else
-          cluster.scan!
-        end
+        cluster.scan!
         retry
       end
     end
