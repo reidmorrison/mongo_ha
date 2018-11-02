80184b9d9531639bc784d6ac454e6d70831006fe
Adrien Rey-Jarthon <adrien.jarthon@dimelo.com>
2018-11-02 09:37:44 +0100

    Compatibility with mongo 2.6

--- lib/mongo_ha/retryable.rb
+++ lib/mongo_ha/retryable.rb
@@ -2,21 +2,21 @@ require 'mongo/retryable'
 
 module Mongo
   module Retryable
-    def legacy_write_with_retry(server = nil)
+    def legacy_write_with_retry(server = nil, session = nil)
       attempt = 0
       begin
         attempt += 1
         yield(server || cluster.next_primary)
       rescue Error::SocketError, Error::SocketTimeoutError => e
         server = nil
-        raise(e) if attempt > cluster.max_read_retries
+        raise(e) if attempt > cluster.max_read_retries || (session && session.in_transaction?)
         log_retry(e)
         cluster.scan!
         retry
       rescue Error::OperationFailure => e
         server = nil
         raise(e) if attempt > cluster.max_read_retries
-        if e.write_retryable?
+        if e.write_retryable? && !(session && session.in_transaction?)
           log_retry(e)
           cluster.scan!
           retry
